106e55899e1739ce0bfd0d9608da463b2a3a0331
Rahul <rahulkum94l94@gmail.com>
2026-04-08 18:45:36 +0530

    feat:added a hanlde connection makin stream-> http req

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream},
+        io::{BufReader,prelude::*}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,5 +8,16 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream:TcpStream){
+    let buf_reader = BufReader::new(&stream);
+    let http_request:Vec<_> = buf_reader
+                                .lines()
+                                .map(|result| result.unwrap())
+                                .take_while(|line| !line.is_empty())
+                                .collect();
+    println!("Request: {http_request:#?}");
 }
